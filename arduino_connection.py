3e364431e2503edb78acd7f7010ab982be8402e0
Chnapak <matej.hladecek@gmail.com>
2026-01-13 11:24:03 +0100

    HTTP requests and added comments

---
+++ arduino_connection.py
@@ -0,0 +1,40 @@
+import json
+import time
+import requests
+
+ESP_IP = "192.168.4.1" # Neměnit
+PING_URL = f"http://{ESP_IP}/ping" # Testování spojení s Arduinem
+DATA_URL = f"http://{ESP_IP}/data" # Posílání kolik má Arduino udělat kroků s motorem. 
+
+def main():
+    try:
+        # 1) Ping
+        r = requests.get(PING_URL, timeout=2)
+        r.raise_for_status()
+        print("Ping OK:", r.text) # pong = dobře.
+
+        # 2) Poslat data v JSON
+        # Můžeš poslat "angle" nebo "steps". Arduino si s tím poradí. Angle je v °
+        payload = {"steps": 8888} # Zpráva k odeslání
+        r = requests.post(DATA_URL, json=payload, timeout=2) # Poslání zprávy
+        r.raise_for_status() # Hodí vyjímku, když není OK. :(
+        print("POST /data OK:", r.text)
+        
+        # Kroky nesmí jít rovnou za sebou. Arduino je přechytralý a vykonává kroky, co nejrychleji.
+        # Např. pokud by přišel předchozí příkaz na 8888 kroků a hned potom -1000, tak udělá jenom 7888 kroků nikoliv 9888.
+        # Proto se hodí sleep.
+        time.sleep(10)
+
+        # Další příklad poslání zprávy
+        payload = {"steps": -1000}
+        r = requests.post(DATA_URL, json=payload, timeout=2)
+        r.raise_for_status()
+        print("POST /data OK:", r.text)
+
+    except requests.exceptions.RequestException as e:
+        # Popíše chybu, když program selže.
+        # Např. Arduino je vyplé, nejsi na wifi, špatně poslaná žádost.
+        print("Request failed:", e)
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -4,59 +4,66 @@ from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 import cv2 as cv
 
-# Initialize webcam (0 = default camera)
+# Tohle je zkušební soubor na testování AI. Neměnil bych ho, ale spíš bych měnil pouze Arduino_connection.
+# Je možné testovat na tvém zařízení.
+
+# Připravit kameru (0 = zvolit základní kameru, 1/2/3/4... = další kamery)
 cam = cv.VideoCapture(0)
 
-# Capture one frame
+# Vyfotit jediný snímek
+# bool ret = vrátil jsi vůbec něco?
+# frame = snímek
 ret, frame = cam.read()
 
 if ret:
-    cv.imshow("Captured", frame)         
-    cv.imwrite("captured_image.png", frame)  
-    cv.waitKey(2000)                      
-    cv.destroyWindow("Captured")       
+    cv.imshow("Captured", frame) # Ukázat okno s fotkou    
+    cv.imwrite("captured_image.png", frame) # Uložit fotku
+    cv.waitKey(2000) # Počkat
+    cv.destroyWindow("Captured") # Vypnout okno s fotkou       
 else:
-    print("Failed to capture image.")
+    print("Failed to capture image.") # ret = false, nemám fotku.
 
-cam.release() 
+cam.release() # Kamera se musí vypnout!!!!
 
-
-# Disable scientific notation for clarity - 167 = 1.67-E2
+# Nepoužívat vědecký zapís čísel v této knihovně. Jde o nastavení, které musí být na začátku spuštění programu.
+# Např. 167 = 1.67-E2 ve vědeckém zápisu
 np.set_printoptions(suppress=True) 
 
-# Load the model
+# Načíst model ze souboru.
 model = load_model("keras_model.h5", compile=False)
 
-# Load the labels
+# Načíst názvy skupin (skupiny odpadků: plast, papír, kov atd.)
 class_names = open("labels.txt", "r").readlines()
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
+# Připraví správný tvar array, který má být vstupem pro AI model.
+# Délka arraye je počet fotek, který určuje první číslo arraye.
+# (počet fotek, počet pixelů do šířky, počet pixelů do výšky, počet hodnot (R,G,B)) =
+# (1, 224, 224, 3)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Replace this with the path to your image
+# Získá si data z obrázku
 image = Image.open("captured_image.png").convert("RGB")
 
-# resizing the image to be at least 224x224 and then cropping from the center
+# Změní velikost obrázku na alespoň 224x224, ale ořezáním ze středu, nikoliv compresí.
 size = (224, 224)
 image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-# turn the image into a numpy array
+# Převede obrázek do arraye správného tvaru.
 image_array = np.asarray(image)
 
-# Normalize the image
+# Normalizuje hodnoty na interval od -1 do 1 místo 0 až 255
+# <0;255> => <-1;1>
 normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-# Load the image into the array
+# Přidá obrázek do dat. Může mít více obrázků.
 data[0] = normalized_image_array
 
-# Predicts the model
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
 
-# Print prediction and confidence score
+prediction = model.predict(data) # Model předpoví 
+index = np.argmax(prediction)   # Najde nejlepší odhad
+class_name = class_names[index] # Najde název nejlepšího odhadu
+confidence_score = prediction[0][index] # Najde procentuální odhad modelu 
+
+# Print názvu a důvery
 print("Class:", class_name[2:], end="")
 print("Confidence Score:", confidence_score)
