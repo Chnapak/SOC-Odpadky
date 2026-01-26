f8529de6288dbccfcca414f24e1340f947620c5e
Chnapak <matej.hladecek@gmail.com>
2026-01-26 09:49:23 +0100

    Non-stop HTTP requests

--- arduino_connection.py
+++ arduino_connection.py
@@ -15,11 +15,11 @@ def main():
 
         # 2) Poslat data v JSON
         # Můžeš poslat "angle" nebo "steps". Arduino si s tím poradí. Angle je v °
-        payload = {"steps": 8888} # Zpráva k odeslání
+        payload = {"steps": 1000} # Zpráva k odeslání
         r = requests.post(DATA_URL, json=payload, timeout=2) # Poslání zprávy
         r.raise_for_status() # Hodí vyjímku, když není OK. :(
         print("POST /data OK:", r.text)
-        
+
         # Kroky nesmí jít rovnou za sebou. Arduino je přechytralý a vykonává kroky, co nejrychleji.
         # Např. pokud by přišel předchozí příkaz na 8888 kroků a hned potom -1000, tak udělá jenom 7888 kroků nikoliv 9888.
         # Proto se hodí sleep.
@@ -28,8 +28,14 @@ def main():
         # Další příklad poslání zprávy
         payload = {"steps": -1000}
         r = requests.post(DATA_URL, json=payload, timeout=2)
-        r.raise_for_status()
+        r.raise_for_status() 
         print("POST /data OK:", r.text)
+
+        # Papír 0°
+        # Plast 72°
+        # Kov 144°
+        # Sklo 216°
+        # Směs 288°
 
     except requests.exceptions.RequestException as e:
         # Popíše chybu, když program selže.
---
+++ complete_program.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+import cv2 as cv
+import json
+import time
+import requests
+
+ESP_IP = "192.168.4.1" # Neměnit
+PING_URL = f"http://{ESP_IP}/ping" # Testování spojení s Arduinem
+DATA_URL = f"http://{ESP_IP}/data" # Posílání kolik má Arduino udělat kroků s motorem.
+ 
+def main():
+    # Config
+    cam = cv.VideoCapture(0)
+    # Nepoužívat vědecký zapís čísel v této knihovně. Jde o nastavení, které musí být na začátku spuštění programu.
+    # Např. 167 = 1.67-E2 ve vědeckém zápisu
+    np.set_printoptions(suppress=True) 
+
+    # Načíst model ze souboru.
+    model = load_model("keras_model.h5", compile=False)
+
+    # Načíst názvy skupin (skupiny odpadků: plast, papír, kov atd.)
+    class_names = open("labels.txt", "r").readlines()
+
+    # Připraví správný tvar array, který má být vstupem pro AI model.
+    # Délka arraye je počet fotek, který určuje první číslo arraye.
+    # (počet fotek, počet pixelů do šířky, počet pixelů do výšky, počet hodnot (R,G,B)) =
+    # (1, 224, 224, 3)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    size = (224, 224)
+
+    while True:
+        # Vyfotit jediný snímek
+        # bool ret = vrátil jsi vůbec něco?
+        # frame = snímek
+        ret, frame = cam.read()
+
+        if ret: 
+            cv.imshow("Captured", frame) # Ukázat okno s fotkou    
+            cv.imwrite("captured_image.png", frame) # Uložit fotku
+            cv.waitKey(2000) # Počkat
+            cv.destroyWindow("Captured") # Vypnout okno s fotkou     
+        else:
+            print("Failed to capture image.") # ret = false, nemám fotku.
+            break
+
+        
+        # Získá si data z obrázku
+        image = Image.open("captured_image.png").convert("RGB")
+
+        # Změní velikost obrázku na alespoň 224x224, ale ořezáním ze středu, nikoliv compresí.
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+        # Převede obrázek do arraye správného tvaru.
+        image_array = np.asarray(image)
+
+        # Normalizuje hodnoty na interval od -1 do 1 místo 0 až 255
+        # <0;255> => <-1;1>
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+        # Přidá obrázek do dat. Může mít více obrázků.
+        data[0] = normalized_image_array
+
+
+        prediction = model.predict(data) # Model předpoví 
+        index = np.argmax(prediction)   # Najde nejlepší odhad
+        class_name = class_names[index] # Najde název nejlepšího odhadu
+        confidence_score = prediction[0][index] # Najde procentuální odhad modelu 
+
+        # Print názvu a důvery
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", confidence_score)
+        angle = None
+        type_of_waste = class_name[2:(len(class_name)-1)]
+        print(type_of_waste)
+
+        if confidence_score <= 0.5:
+            angle = -60
+            print("Model si neni jisty")
+        else:
+            match type_of_waste:
+                case "Plast":
+                    angle = 0
+                case "Sklo":
+                    angle = 60
+                case "Papir":
+                    angle = 120
+                case "Kovy":
+                    angle = 180
+                case "Smesi":
+                    angle = -120
+        
+        if angle == None:
+            print("nebyla prirazena validni skupina")
+            break
+        
+        try:
+            # 1) Poslat data v JSON
+            # Můžeš poslat "angle" nebo "steps". Arduino si s tím poradí. Angle je v °
+            payload = {"angle": angle} # Zpráva k odeslání
+            r = requests.post(DATA_URL, json=payload, timeout=2) # Poslání zprávy
+            r.raise_for_status() # Hodí vyjímku, když není OK. :(
+            print("POST /data OK:", r.text)
+            
+            # Kroky nesmí jít rovnou za sebou. Arduino je přechytralý a vykonává kroky, co nejrychleji.
+            # Např. pokud by přišel předchozí příkaz na 8888 kroků a hned potom -1000, tak udělá jenom 7888 kroků nikoliv 9888.
+            # Proto se hodí sleep
+
+        except requests.exceptions.RequestException as e:
+            # Popíše chybu, když program selže.
+            # Např. Arduino je vyplé, nejsi na wifi, špatně poslaná žádost.
+            print("Request failed:", e)
+            
+
+
+if __name__ == "__main__":
+    main()
